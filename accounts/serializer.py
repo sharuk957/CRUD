0bb3c4b8e95f0858be1da5bb6c205fc5901edebf
sharuk957 <muhammedsharuk101@gmail.com>
2022-08-26 15:26:35 +0530

    input validation is included

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -1,13 +1,56 @@
+from unittest.util import _MAX_LENGTH
 from .models import User
 from rest_framework import serializers
+from django.core.validators import RegexValidator
+from django.forms import ValidationError
+
+
+def name_validator(value,message):
+    if len(value) < 0:
+        raise ValidationError(f"{message} cannot be empty")
+    validator = RegexValidator(r'^[a-zA-Z]*$',f"{message} contain only character")
+    return validator(value)
+
+
+def email_validator(value,message):
+    if len(value) < 0:
+        raise ValidationError(f"{message} cannot be empty")
+    validator = RegexValidator(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b',f"invalid {message} format")
+    return validator(value)
+
+
+def password_validator(value,message):
+    if len(value) < 0:
+        raise ValidationError(f"{message} cannot be empty")
+    validator = RegexValidator(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,}$',f"{message} contain Minimum five characters, at least one letter, one number and one special character:")
+    return validator(value)
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
+    def validate_email(self,value):
+        email_validator(value,'email')
+        return value
+    
+    def validate_first_name(self,value):
+        name_validator(value,'first_name')
+        return value
+
+    def validate_last_name(self,value):
+        name_validator(value,'last_name')
+        return value
+
+    def validate_password(self,value):
+        password_validator(value,'password')
+        return value
+    
     class Meta:
         model = User
-        fields = '__all__'
-    
+        fields = ['user_type', 'email', 'first_name', 'last_name', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
     def create(self, validated_data):
         user_type = validated_data.get('user_type')
         if user_type == 'S':
--- vehicles/serializer.py
+++ vehicles/serializer.py
@@ -1,10 +1,37 @@
+from email import message
+import re
+from django.forms import ValidationError
 from rest_framework import serializers
 from .models import Vehicle
+from django.core.validators import RegexValidator
 
-
+def regex_validator(value,message,regex):
+    if len(value) < 0:
+        raise ValidationError(f"{message} cannot be empty")
+    validator = RegexValidator(rf"{regex}",f"{message} contain only alphanumeric character")
+    return validator(value)
 
 class VehicleSerializer(serializers.ModelSerializer):
+
+    def validate_vehicle_number(self, value):
+        regex = '^[0-9a-zA-Z]*$'
+        field_name = "vehicle_number"
+        regex_validator(value,field_name,regex)
+        return value
+    
+    def validate_vehicle_name(self, value):
+        regex = '^[0-9a-zA-Z-()]+(\s+[-0-9a-zA-Z-()]+)*$'
+        field_name = "vehicle_number"
+        regex_validator(value,field_name,regex)
+        return value
+    
+    def validate_vehicle_description(self, value):
+        regex = '^[0-9a-zA-Z-()]+(\s+[-0-9a-zA-Z-()]+)*$'
+        field_name = "vehicle_number"
+        regex_validator(value,field_name,regex)
+        return value
 
     class Meta:
         model = Vehicle
         fields = '__all__'
+    
